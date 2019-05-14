a990639094441a39d01a30c9dab3a1e78ab63a24
MatheusEstanislau <matheus.estanislau7@gmail.com>
2019-05-14 14:59:00 -0300

    Aula 5: Iterando de Maneira diferente

---
+++ Python3: Parte 1/adivinhacao.py
@@ -0,0 +1,31 @@
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+total_de_tentativas = 3
+rodada = 1
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}" .format(rodada, total_de_tentativas))
+    chute = int(input("Digite um numero entre 1 e 100: "))
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100! ")
+        continue
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if acertou:
+        print("Parabéns! Você acertou!")
+        break
+    else:
+        if maior:
+            print("O seu chute foi maior que o número secreto!")
+        elif menor:
+            print("O seu chute foi menor que o número secreto!")
+
+print("Fim do jogo")
