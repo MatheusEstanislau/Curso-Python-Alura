3e2a27b52e6b3a601c74f0480037a3373a2187d1
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-05-16 19:13:09 -0300

    Curso Python: Parte 2. Aula 1: Preparando o jogo da forca

---
+++ Python3: Parte 2/Forca.py
@@ -0,0 +1,19 @@
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_chave = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+
+        print("jogando...")
+
+    print("Fim do jogo")
+
+if __name__ == "__main__":
+    jogar()
