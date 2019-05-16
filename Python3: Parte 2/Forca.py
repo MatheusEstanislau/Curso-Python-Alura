613c3332c892984d11e76eb6c2cfd46b9e8607e2
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-05-16 20:15:37 -0300

    Aula 3: Conhecendo e trabalhando com Listas

--- Python3: Parte 2/Forca.py
+++ Python3: Parte 2/Forca.py
@@ -6,23 +6,37 @@ def jogar():
     palavra_chave = "banana"
     enforcou = False
     acertou = False
+    letras_acertadas = []
+
+    count = len(palavra_chave)
+
+    for i in range(1,len(palavra_chave) + 1):
+        letras_acertadas.append("_")
+
+    print(letras_acertadas)
 
     while not acertou and not enforcou:
+
 
         chute = input("Qual letra? ")
         chute = chute.strip()
 
         index = 0
+
         for letra in palavra_chave:
             if chute.upper() == letra.upper():
+                letras_acertadas[index] = chute
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
-            print("jogando...")
+
+        if (not "_" in letras_acertadas):
+            acertou = True
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
 
 if __name__ == "__main__":
     jogar()
-
