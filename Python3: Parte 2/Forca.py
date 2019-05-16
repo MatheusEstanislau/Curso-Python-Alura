6e56c32314e387bc878180e9c2a671e60e527a61
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-05-16 19:21:52 -0300

    Aula 2: Manipulando Strings

--- Python3: Parte 2/Forca.py
+++ Python3: Parte 2/Forca.py
@@ -1,19 +1,28 @@
-
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
     palavra_chave = "banana"
-
     enforcou = False
     acertou = False
 
-    while(not acertou and not enforcou):
+    while not acertou and not enforcou:
 
-        print("jogando...")
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_chave:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+            print("jogando...")
 
     print("Fim do jogo")
 
+
 if __name__ == "__main__":
     jogar()
+
