e95112d4921461ca522e33f4b714340e615c795b
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-05-17 01:06:27 -0300

    Aula 5: Implementando o encerramento do jogo

--- Python3: Parte 2/Forca.py
+++ Python3: Parte 2/Forca.py
@@ -4,22 +4,28 @@ def jogar():
     print("*********************************")
 
     palavra_chave = "banana"
-    enforcou = False
-    acertou = False
     letras_acertadas = []
 
-    count = len(palavra_chave)
+    print("Qual nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
 
-    for i in range(1,len(palavra_chave) + 1):
+    nivel = int(input("Defina o nível: "))
+
+    if nivel == 1:
+        total_de_tentativas = 20
+    elif nivel == 2:
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 5
+
+    for i in range(1, len(palavra_chave) + 1):
         letras_acertadas.append("_")
 
     print(letras_acertadas)
 
-    while not acertou and not enforcou:
-
-
-        chute = input("Qual letra? ")
-        chute = chute.strip()
+    for rodada in range(1, total_de_tentativas + 1):
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+        chute = input("Digite um numero entre 1 e 100: ")
 
         index = 0
 
@@ -29,13 +35,13 @@ def jogar():
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
-
-        if (not "_" in letras_acertadas):
-            acertou = True
+        if "_" not in letras_acertadas:
+            print("Parabéns você acertou!")
+            break
 
         print(letras_acertadas)
 
-    print("Fim do jogo")
+    print("Que pena você perdeu! A palavra secreta era {}".format(palavra_chave.upper()))
 
 
 if __name__ == "__main__":
